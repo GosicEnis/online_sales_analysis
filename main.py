74b9d2a8129eb97f6b6dd0e329e49ce5efc8c60e
Gosić Enis <gosicenis21@gmail.com>
2025-04-23 13:15:02 +0200

    Ažurirana cijena proizvoda i uklonjen prikaz inventara

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from product_manager import ProductManager
 
 # Create a ProductManager instance and add a few arbitrary products;
 manager = ProductManager()
-manager.add_product(Product("Laptop", 1000, 3))
-manager.add_product(Product("Mouse", 25.50, 15))
-manager.add_product(Product("Keyboard", 50.45, 5))
-manager.add_product(Product("Monitor", 150, 7))
+manager.add_product(Product("Laptop", 1000, 4))
+manager.add_product(Product("Mouse", 25.50, 10))
+manager.add_product(Product("Keyboard", 50.45, 8))
+manager.add_product(Product("Monitor", 150, 5))
 
 # Display of products and total inventory value
-manager.display_all_products()
-print(f"\nTotal inventory value: {manager.total_value()}")
+# manager.display_all_products()
+# print(f"\nTotal inventory value: {manager.total_value()}")
